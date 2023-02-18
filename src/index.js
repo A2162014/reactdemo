3ebf5fe183d9a3d03f100f359234048a5245de11
Ashvath <ashvath.suresh@btech.christuniversity.in>
2023-02-18 13:31:15 +0530

    update-1

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ const uid = "React" //strings
 const place = "India" //strings
 const num1 = prompt("Enter number 1") //number/integer
 const num2 = prompt("Enter number 2") //number/integer
-const total = num1 + num2
-const avg = Number((num1 + num2) / 2)
+const total = Number(num1) + Number(num2)
+const avg = (Number(num1) + Number(num2)) / 2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <h1>Welcome to React world</h1>
+    // [
+    //     <h1>Welcome to React world</h1>,
+    //     <h2>This is square method!</h2>
+    // ]
+    // <div>
+    //      <h1>Welcome to React world</h1>
+    //      <h2>This is div method!</h2>
+    //      <h3>not highly recommended</h3>
+    // </div>
+    <>
+        <h1>Welcome to {uid} world</h1>
+        <h2>This is fragment method!</h2>
+        <h3>highly recommended</h3>
+        <h1>Welcome to {place}!</h1>
+        <h2>Number 1 is {num1}.</h2>
+        <h2>Number 2 is {num2}.</h2>
+        <h2>Total is {total}.</h2>
+        <h2>Average is {avg}.</h2>
+    </>
 )
