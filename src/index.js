770ea75c0429047882480a00cebc9a66449da3e1
Ashvath <ashvath.suresh@btech.christuniversity.in>
2023-02-19 13:38:15 +0530

    export and import values

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Header from './Header.js'
-import Body from './Body.js'
-import Footer from './Footer.js'
+// import Header from './Header.js'
+// import Body from './Body.js'
+// import Footer from './Footer.js'
+// import course, { topics, duration, myName, myLastName } from "./Body";
+import * as body from './Body'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <>
-        <Header />
+        {/* <h3>You selected {course}</h3>
+        <h3>Topics are {topics}</h3>
+        <h3>Duration is {duration}</h3>
+        <h3>My name is {myName()}</h3>
+        <h3>My last name is {myLastName()}</h3> */}
+        <h3>You selected {body.default}</h3>
+        <h3>Topics are {body.topics}</h3>
+        <h3>Duration is {body.duration}</h3>
+        <h3>My name is {body.myName()}</h3>
+        <h3>My last name is {body.myLastName()}</h3>
+        {/* <Header />
         <Body />
-        <Footer />
+        <Footer /> */}
     </>
 )
