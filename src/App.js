63eabb7add414b0db78988ac6b24019e448b39c9
Ashvath <ashvath.suresh@btech.christuniversity.in>
2023-02-27 20:49:19 +0530

    Redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import players from './Players.json'
-import './style.css'
+// import players from './Players.json'
+// import './style.css'
+
+// function App() {
+//     return (
+//         <>
+//             <h2>Importing JSON data</h2>
+//             {players.map((player) => {
+//                 return (
+//                     <>
+//                         <div>
+//                             <br></br>
+//                             <img src={player.pic} alt=''></img>
+//                             <h3>{player.name}</h3>
+//                         </div>
+//                     </>
+//                 )
+//             })}
+//         </>
+//     )
+// }
+// export default App
+
+// import { createStore } from 'redux'
+import { legacy_createStore as createStore } from 'redux'
+
+const initialState = "Hello Redux"
+const reducer = (state = initialState, action) => {
+    return state
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 function App() {
     return (
-        <>
-            <h2>Importing JSON data</h2>
-            {players.map((player) => {
-                return (
-                    <>
-                        <div>
-                            <br></br>
-                            <img src={player.pic} alt=''></img>
-                            <h3>{player.name}</h3>
-                        </div>
-                    </>
-                )
-            })}
-        </>
+        <div>
+            <h1>Redux</h1>
+            <h3>{store.getState()}</h3>
+        </div>
     )
 }
+
 export default App
